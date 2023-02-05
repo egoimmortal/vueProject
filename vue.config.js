bc7bf1ead225adf1e4a585dff016c54e1c95a995
egoimmortal <want0945@gmail.com>
2023-02-06 00:42:50 +0800

    修改部屬設定

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/vueProject/'
+  publicPath: process.env.NODE_ENV == 'development' ? '/' : '/vueProject/'
 })
