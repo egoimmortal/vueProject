f6899820e277cfedf088d668c6957cb753398812
egoimmortal <want0945@gmail.com>
2023-02-17 00:55:10 +0800

    修改about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+	<div class="about">
+		<h1>This is an about me page</h1>
+		<h1>關於我的頁面</h1>
+		<div class="content">
+			<div class="left">
+				timeline
+			</div>
+			<div class="right">
+				<div>
+					1. 工作:
+				</div>
+				<div>
+					2. 語言:
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+.content{
+	display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+</style>
