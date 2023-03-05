9acea2458d78fde3120809e3fc1df751ba1cfafa
egoimmortal <want0945@gmail.com>
2023-03-06 00:03:59 +0800

    修改sidebar測試內容

--- src/components/LeftBarList.vue
+++ src/components/LeftBarList.vue
@@ -1,16 +1,22 @@
 <template>
     <div class="left-bar-parent col-3">
-        leftbar
+        <div v-for="(data, index) in sidebarData" :key="index">
+            <div>
+                {{ data.name }}
+            </div>
+        </div>
     </div>
 </template>
 
 <script lang="ts" setup>
-
+import sidebarData from '@/assets/json/sidebar.json'
+console.log(sidebarData);
 </script>
 
 <style lang="scss" scoped>
 .left-bar-parent{
 	display: flex;
 	justify-content: center;
+    flex-direction: column;
 }
 </style>
