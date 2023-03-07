3d700e0e16e9e767e70a6ec2e64c9546efeba694
egoimmortal <want0945@gmail.com>
2023-03-08 01:42:45 +0800

    新增itemlist和item

---
+++ src/components/Item/ItemList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="col">
+        <ItemCom></ItemCom>
+        props
+    </div>
+</template>
+
+<script lang="ts" setup>
+import ItemCom from './ItemCom.vue';
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    name: String,
+    color: String
+})
+
+console.log(props);
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
